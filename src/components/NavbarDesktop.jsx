ede6790063567fe01c361eadef157892209251b7
Soham Mitra <mitra.soham53@gmail.com>
2023-07-02 15:30:54 +0530

    initialized two different navbars for different screen sizes

--- src/components/NavbarDesktop.jsx
+++ src/components/NavbarDesktop.jsx
@@ -3,28 +3,38 @@ import Toolbar from "@mui/material/Toolbar";
 import navButtonComp from "../styles/navButtonComp";
 import ThemeToggleSwitch from './ThemeToggleSwitch';
 import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import AppBar from "@mui/material/AppBar";
 const NavbarDesktop = () => {
     return (
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography color="" variant="h4">Soham&apos;s portfolio</Typography>
-            <List component="nav">
-                <ListItem component="div">
-                    <ThemeToggleSwitch />
-                    <ListItemButton component="a" href='/about' sx={navButtonComp} >
-                        <ListItemText primary="About Me" />
-                    </ListItemButton>
-                    <ListItemButton component="a" href='/skills' sx={navButtonComp} >
-                        <ListItemText primary="Skills" />
-                    </ListItemButton>
-                    <ListItemButton component='a' href='/projects' sx={navButtonComp}>
-                        <ListItemText primary="Projects" />
-                    </ListItemButton>
-                    <ListItemButton component='a' href='/projects' sx={navButtonComp}>
-                        <ListItemText primary="Contact Me!" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-        </Toolbar>
+        <>
+            <AppBar position="static" sx={{ display: 'flex', justifyContent: 'space-between', border: 3, borderRadius: 5, width: '100%' }}>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography color="" variant="h4">Soham&apos;s portfolio</Typography>
+                    <List component="nav">
+                        <ListItem component="div">
+                            <ThemeToggleSwitch />
+                            <ListItemButton component="a" href='/about' sx={navButtonComp} >
+                                <ListItemText primary="About Me" />
+                            </ListItemButton>
+                            <ListItemButton component="a" href='/skills' sx={navButtonComp} >
+                                <ListItemText primary="Skills" />
+                            </ListItemButton>
+                            <ListItemButton component='a' href='/projects' sx={navButtonComp}>
+                                <ListItemText primary="Projects" />
+                            </ListItemButton>
+                            <ListItemButton component='a' href='/projects' sx={navButtonComp}>
+                                <ListItemText primary="Contact Me!" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Toolbar>
+            </AppBar>
+        </>
     );
 }
 export default NavbarDesktop;
+
+{/*
+
+*/
+}
--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -3,20 +3,29 @@ import Toolbar from "@mui/material/Toolbar";
 import ThemeToggleSwitch from './ThemeToggleSwitch';
 import { List, ListItem, IconButton } from '@mui/material';
 import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Drawer from "@mui/material/Drawer";
 
 const NavbarMobile = () => {
     return (
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography color="" variant="h4">Soham&apos;s portfolio</Typography>
-            <List component="nav">
-                <ListItem component="div">
-                    <ThemeToggleSwitch />
-                    <IconButton>
-                        <MenuIcon />
-                    </IconButton>
-                </ListItem>
-            </List>
-        </Toolbar>
+        <>
+            <AppBar position="static" sx={{ display: 'flex', justifyContent: 'space-between', border: 3, borderRadius: 5, width: '100%' }}>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography color="" variant="h4">Soham&apos;s portfolio</Typography>
+                    <List component="nav">
+                        <ListItem component="div">
+                            <ThemeToggleSwitch />
+                            <IconButton>
+                                <MenuIcon />
+                            </IconButton>
+                        </ListItem>
+                    </List>
+                </Toolbar>
+            </AppBar>
+            <Drawer open={open}>
+
+            </Drawer>
+        </>
     );
 };
 
