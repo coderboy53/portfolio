6a24953dc9e153efb430719c2ea5cd7365db82ad
Soham Mitra <mitra.soham53@gmail.com>
2023-07-03 03:35:30 +0530

    added stuff

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -17,13 +17,13 @@ const AboutMe = () => {
                     </Grid>
                     <Grid container item spacing={3} md lg direction={'column'}>
                         <Grid item >
-                            <Paper elevation={5} sx={{ p: 5 }}>
+                            <Paper elevation={5} sx={{ p: 3 }}>
                                 <Typography variant="h4" gutterBottom>Hey there! This is me</Typography>
-                                <Typography variant="subtitle1">Soham Mitra, an aspiring Software Developer</Typography>
+                                <Typography variant="subtitle1">Soham Mitra, an aspiring Software Developer. This page is still under development. Come back later.😁</Typography>
                             </Paper>
                         </Grid>
                         <Grid item >
-                            <Paper elevation={5} sx={{ p: 5 }}>
+                            <Paper elevation={5} sx={{ p: 3 }}>
                                 <Typography variant="h4" gutterBottom>About Me</Typography>
                                 <Typography variant="body1">I am a Computer Science and Engineering Under Graduate. My interests in Computer Science are in the fields of Software Development, Web Development. I live in Kolkata, India. My hobbies are working out and listening to music.</Typography>
                             </Paper>
