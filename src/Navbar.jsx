d222244df0ed2ccbd0b4e5eed0b7893a0c2e41a0
Soham Mitra <mitra.soham53@gmail.com>
2023-07-02 15:25:16 +0530

    preparing navbar to change in different screens with two different navbars, for mobile and desktop

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,15 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box"
-import AppBar from "@mui/material/AppBar";
 import NavbarDesktop from "./components/NavbarDesktop";
 import NavbarMobile from "./components/NavbarMobile";
+import { useState } from "react";
+
 const Navbar = () => {
     return (
         <>
             <CssBaseline />
             <Box sx={{m: 2}}>
-                <AppBar position="static" sx={{ display: 'flex', justifyContent: 'space-between', border: 3, borderRadius: 5, width: '100%'}}>
-                    <NavbarMobile />
-                </AppBar>
+                <NavbarDesktop />
             </Box>
         </>
     );
