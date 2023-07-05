fcd5509a2095f2869a163ba76145cead991d7889
Soham Mitra <mitra.soham53@gmail.com>
2023-07-06 02:06:05 +0530

    added theme changing functionality using cookies temporarily

--- src/components/NavbarDesktop.jsx
+++ src/components/NavbarDesktop.jsx
@@ -1,10 +1,14 @@
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import navButtonComp from "../styles/navButtonComp";
-import ThemeToggleSwitch from './ThemeToggleSwitch';
+import ThemeToggleSwitch from '../assets/ThemeToggleSwitch';
 import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import AppBar from "@mui/material/AppBar";
+import themeSetter from "../assets/themeSetter";
+import { Cookies } from "react-cookie";
 const NavbarDesktop = () => {
+    const cookies = new Cookies;
+    let set = cookies.get('theme');
     return (
         <>
             <AppBar position="static" sx={{ display: 'flex', justifyContent: 'space-between', border: 3, borderRadius: 5, width: '100%' }}>
@@ -12,7 +16,7 @@ const NavbarDesktop = () => {
                     <Typography color="" variant="h4">Sam&apos;s portfolio</Typography>
                     <List component="nav">
                         <ListItem component="div">
-                            <ThemeToggleSwitch />
+                            <ThemeToggleSwitch onChange={themeSetter} checked={set==='dark'}/>
                             <ListItemButton component="a" href='./about' sx={navButtonComp} >
                                 <ListItemText primary="About Me" />
                             </ListItemButton>
