21d9a2128aa49b2aa3a1cb7965f33d4aa6724e01
Soham Mitra <mitra.soham53@gmail.com>
2023-07-05 14:32:57 +0530

    set up navbar to change based on screen size between desktop and mobile

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,13 +2,22 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box"
 import NavbarDesktop from "./components/NavbarDesktop";
 import NavbarMobile from "./components/NavbarMobile";
+import { useMediaQuery } from "@mui/material";
 
 const Navbar = () => {
+    const desktop = useMediaQuery('(min-width:961px');
     return (
         <>
             <CssBaseline />
             <Box sx={{ m: 2 }}>
-                <NavbarMobile />
+                {
+                    desktop? (
+                        <NavbarDesktop />
+                    ):
+                    (
+                        <NavbarMobile />
+                    )
+                }
             </Box>
         </>
     );
