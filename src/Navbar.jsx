6f40e43b8b0c336b1397171e137f2b4bbd316695
Soham Mitra <mitra.soham53@gmail.com>
2023-07-05 11:50:21 +0530

    setting up drawer for mobile navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,14 +2,13 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box"
 import NavbarDesktop from "./components/NavbarDesktop";
 import NavbarMobile from "./components/NavbarMobile";
-import { useState } from "react";
 
 const Navbar = () => {
     return (
         <>
             <CssBaseline />
-            <Box sx={{m: 2}}>
-                <NavbarDesktop />
+            <Box sx={{ m: 2 }}>
+                <NavbarMobile />
             </Box>
         </>
     );
