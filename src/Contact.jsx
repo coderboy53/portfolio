c34cc66c88f3910e1a9c98b8accf483136c0fa61
Soham Mitra <mitra.soham53@gmail.com>
2023-07-29 11:06:44 +0530

    updated icons on contact, started work on skills project

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,7 @@ import Paper from "@mui/material/Paper"
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box"
 import { List, ListItem, ListItemButton, ListItemIcon } from "@mui/material";
-import { GitHub, LinkedIn, Twitter } from "@mui/icons-material";
+import { GitHub, LinkedIn, Twitter, Email } from "@mui/icons-material";
 const Contact = () => {
     return (
         <>
@@ -11,25 +11,30 @@ const Contact = () => {
             <Box sx={{ p: 2, m: 2, justifyContent: 'center' }}>
                 <Paper sx={{ p: 3 }} elevation={5}>
                     <Typography variant="h4" gutterBottom>Contact Me!</Typography>
-                    <Typography variant="h6">
+                    <Typography variant="h5">
                         Hey, I would like to get to know you guys. You can connect with me at my socials through the following links
                     </Typography>
                     <Box sx={{display: 'flex', justifyContent: 'center'}}>
                         <List sx={{ width:'50%',justifyContent: 'space-between' }}>
                             <ListItem alignItems="center">
-                                <ListItemButton alignItems="center" component='a' href="https://github.com/coderboy53" sx={{ width: '2%', justifyContent:'center'  }}>
+                                <ListItemButton alignItems="center" component='a' href="https://github.com/coderboy53" sx={{ width: '4%', justifyContent:'center', height:'8%'  }}>
                                     <ListItemIcon sx={{justifyContent:'center'}}>
-                                        <GitHub fontSize="large"/>
+                                        <GitHub sx={{fontSize:70}}/>
                                     </ListItemIcon>
                                 </ListItemButton>
-                                <ListItemButton alignItems="center" component="a" href="https://twitter.com/coderboy53" sx={{ width: '2%', justifyContent:'center'  }}>
+                                <ListItemButton alignItems="center" component="a" href="https://twitter.com/coderboy53" sx={{ width: '4%', justifyContent:'center', height:'8%'  }}>
                                     <ListItemIcon sx={{justifyContent:'center'}}>
-                                        <Twitter fontSize="large"/>
+                                        <Twitter sx={{fontSize:70}}/>
                                     </ListItemIcon>
                                 </ListItemButton>
-                                <ListItemButton component="a" href="https://linkedin.com/in/soham-mitra53/" sx={{ width: '2%', justifyContent:'center' }}>
+                                <ListItemButton component="a" href="https://linkedin.com/in/soham-mitra53/" sx={{ width: '4%', justifyContent:'center', height:'8%' }}>
                                     <ListItemIcon sx={{justifyContent:'center'}}>
-                                        <LinkedIn fontSize="large"/>
+                                        <LinkedIn sx={{fontSize:70}}/>
+                                    </ListItemIcon>
+                                </ListItemButton>
+                                <ListItemButton component="a" href="mailto:mitra.soham53@gmail.com" sx={{ width: '4%', justifyContent:'center', height:'8%' }}>
+                                    <ListItemIcon sx={{justifyContent:'center'}}>
+                                        <Email sx={{fontSize:70}}/>
                                     </ListItemIcon>
                                 </ListItemButton>
                             </ListItem>
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,8 +1,19 @@
+import { CssBaseline, Grid, Paper, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
 const Skills = () => {
     return (
-       <>
-            <h1>Skills</h1>
-       </> 
+        <>
+            <CssBaseline />
+            <Box sx={{ p: 2, m: 2, justifyContent: 'center' }}>
+                <Grid container spacing={3}>
+                    <Paper sx={{ p: 3 }} elevation={5}>
+                        <Typography variant="h6">
+                            Programming Languages
+                        </Typography>
+                    </Paper>
+                </Grid>
+            </Box>
+        </>
     );
 
 }
